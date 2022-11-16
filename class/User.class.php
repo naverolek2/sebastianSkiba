47f527211bba9dad248b98090293e5f75d4661d4
Sebastian <sebaskiba11@gmail.com>
2022-11-16 14:28:54 +0100

    ogarnięcie kodu

--- class/User.class.php
+++ class/User.class.php
@@ -13,13 +13,42 @@ class User {
         $this->firstName = "";
         $this->lastName = "";
     }
-    public function register() {
+    public function register() : bool{
         $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
         $q = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
         $db = new mysqli('localhost', 'root', '', 'loginformat');
         $preparedQuery = $db->prepare($q);
         $preparedQuery->bind_param('ssss', $this->login, $passwordHash, $this->firstName, $this->lastName);
+        $result = $preparedQuery->execute();
+        return $result;
+        
+    }
+    public function logowanie() :bool {
+        $q = "SELECT * FROM user WHERE login = ? LIMIT 1";
+        $db = new mysqli('localhost', 'root', '', 'loginformat');
+        $preparedQuery = $db->prepare($q);
+        $preparedQuery->bind_param('s', $this->login);
         $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
+        if($result->num_rows == 1) {
+        $row = $result->fetch_assoc();    
+        $passwordHash = $row['password'];
+        if(password_verify($this->password, $passwordHash)) {
+           
+            $this->id = $row['id'];
+            $this->firstName = $row['firstName'];
+            $this->lastName = $row['lastName'];
+            return true;
+        }
+        else {
+            echo "Błędny login lub hasło";
+            return false;
+        }
+        }
+        
+        else {
+            return false;
+        }
         
     }
 }
--- test.php
+++ test.php
@@ -2,7 +2,20 @@
     require_once('class/User.class.php');
 
     $user = new User('jkowalski', 'haslo');
-    $user->register();
+    /*
+    if($user->register()) {
+        echo "Zarejestrowano poprawnie";
+    }
+    else {
+        echo " BŁąd";
+    }
+    */
+    if($user->logowanie()) {
+        echo "Zalogowano pomyślnie";
+    }
+    else {
+        echo "Błędny login lub hasło";
+    }
     echo '<pre>';
     var_dump($user);
 ?>
