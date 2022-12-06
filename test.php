ce0a8439042cba288a7561ed2f6d0c4c85f87678
Sebastian Skiba <sebastian.skiba@teb.edu.pl>
2022-12-06 21:45:17 +0100

    post/ ...

--- test.php
+++ test.php
@@ -10,11 +10,15 @@ use Steampixel\Route;
     });
     
     Route::add('/login', function() {
-        echo "Strona logowania";
+            
+        global $twig;
+        $twig->display('login.html.twig');
+
     });
 
     Route::add('/register', function() {
-        echo "Strona rejestracji";
+        global $twig;
+        $twig->display('register.html.twig');
     });
     
     Route::run('/user');
